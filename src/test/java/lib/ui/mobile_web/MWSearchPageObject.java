ea3276894b0776412c894deacfddce635ef6afa4
lpanchenko <liuba.panchenko@gmail.com>
2021-02-27 02:00:29 -0500

    Fix locator

--- src/test/java/lib/ui/mobile_web/MWSearchPageObject.java
+++ src/test/java/lib/ui/mobile_web/MWSearchPageObject.java
@@ -12,7 +12,7 @@ public class MWSearchPageObject extends SearchPageObject {
         SEARCH_RESULT_ARTICLE = "css:ul.page-list>li.page-summary";
         SEARCH_RESULT_ARTICLE_TITLE_AND_DESCRIPTION_TPL = "xpath://*[contains(@data-title,'{TITLE}')]/following::*[contains(text(),'{DESCRIPTION}')]";
         SEARCH_RESULT_ARTICLE_DESCRIPTION_CONTAINS_TPL = "xpath://*[@class='wikidata-description'][contains(text(),'{SUBSTRING}')]";
-        SEARCH_RESULT_ARTICLE_TITLE_CONTAINS_TPL = "xpath://*[@class='page-summary'][contains(@title,'{SUBSTRING}')]";
+        SEARCH_RESULT_ARTICLE_TITLE_CONTAINS_TPL = "xpath://*[contains(@class, 'page-summary')][contains(@title,'{SUBSTRING}')]";
         SEARCH_RESULT_NO_RESULTS_FOUND = "xpath://*[contains(text(),'No page with this title.')]";
         SEARCH_RESULT_EMPTY_CONTAINER = "css:p.without-results";
         SEARCH_CANCEL_BUTTON = "xpath://div[@class='header-action']//button[@type='button'][contains(text(),'Close')]";
